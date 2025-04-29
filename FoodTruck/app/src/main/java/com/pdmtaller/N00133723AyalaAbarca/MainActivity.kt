0f657493654d771aeb5c1dbb5dd364970d168bf8
NesjsjsjsjsjsA <00133723@uca.edu.sv>
2025-04-29 14:26:16 -0600

    Archivo corregido, 0%

---
+++ FoodTruck/app/src/main/java/com/pdmtaller/N00133723AyalaAbarca/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.pdmtaller.N00133723AyalaAbarca
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pdmtaller.N00133723AyalaAbarca.ui.theme.FoodSpot_By_NestorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodSpot_By_NestorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FoodSpot_By_NestorTheme {
+        Greeting("Android")
+    }
+}
